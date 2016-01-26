5bfaeeb97e143c37886ba469b918e2c3d9022d29
Ryuta Kamizono <kamipo@gmail.com>
2016-01-26 09:47:59 +0900

    Make default to `@prepared_statements = false` in `Mysql2Adapter`
    Prepared statements in mysql2 still have GC issue.
    https://github.com/brianmario/mysql2/issues/694

--- activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
@@ -37,6 +37,7 @@ module ActiveRecord
 
       def initialize(connection, logger, connection_options, config)
         super
+        @prepared_statements = false unless config.key?(:prepared_statements)
         configure_connection
       end
 
